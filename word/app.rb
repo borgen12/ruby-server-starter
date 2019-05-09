4b0cafc355ff2953908a924f5ed59551a329f2db
Chase Linzmeyer <chaselinzmeyer@MacBook-Pro-5.local>
2019-05-09 12:07:06 -0500

    PARANOID

--- word/app.rb
+++ word/app.rb
@@ -1,19 +1,21 @@
 #main app file
 
 require 'sinatra'
+require 'json'
 require 'sinatra/activerecord'
 require './config/environments' #database configuration
-require './models/model'        #Model class
+require './models/owner'        #Model class
 
-get '/' do
-	erb :index
+get '/owners' do
+	@owners = Owner.all
+	@owners.to_json
 end
 
-post '/submit' do
-	@model = Model.new(params[:model])
-	if @model.save
-		redirect '/models'
-	else
-		"Sorry, there was an error!"
-	end
-end
+# post '/submit' do
+# 	@model = Model.new(params[:model])
+# 	if @model.save
+# 		redirect '/models'
+# 	else
+# 		"Sorry, there was an error!"
+# 	end
+# end
