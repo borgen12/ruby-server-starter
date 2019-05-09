caae23345bbc559e37cfa8d5d211f0a130f15bda
Amareya Allen-Dabney <a.allendabney@gmail.com>
2019-05-09 13:28:37 -0500

    pushing to compare

--- word/app.rb
+++ word/app.rb
@@ -7,15 +7,23 @@ require './models/model'        #Model class
 require 'json'
 
 get '/pet' do
-	@pet = Pets.all
+	# @pet = Pets.find_by_sql('SELECT * FROM pets JOIN owners ON pets.owner_id = owners.id;')
+	@pet = Pets.select("pets.*, owners.*").joins("JOIN owners ON pets.owner_id = owners.id;")
 	@pet.to_json
 end
 
-# post '/submit' do
-# 	@model = Model.new(params[:model])
-# 	if @model.save
-# 		redirect '/models'
-# 	else
-# 		"Sorry, there was an error!"
-# 	end
-# end
+post '/submit' do
+	@newpet = Pets.new(params[:model])
+	if @newpet.save
+		redirect '/pets'
+	else
+		"Sorry, there was an error adding the pet!"
+	end
+end
+
+delete '/pet/:id' do
+	@pet_object = Pet.delete(params[:id])
+	@pet_object.to_json
+end
+
+	
--- word/models/model.rb
+++ word/models/model.rb
@@ -1,2 +1,7 @@
 class Pets < ActiveRecord::Base
+    belongs_to :owners
+end 
+
+class Owners < ActiveRecord::Base
+    has_many :pets
 end 
